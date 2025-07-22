1268f43b72a4aa14ca3db59c103089bfae4a15ce
Melissa Boutlendj <melissaboutlendj@gmail.com>
2025-07-22 07:23:45 +0200

    project folder's structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author='Melissa Boutlendj',
+    author_email= 'melissaboutlendj@gmail.com',
+    package= find_packages(),
+    install_requires = []
+)
